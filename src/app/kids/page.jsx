a7e4023a124f776e8692030a56022da3c7d11f59
Ayan <sujon.seo.bd@gmail.com>
2025-11-23 21:56:31 +0600

    added all category products

--- src/app/kids/page.jsx
+++ src/app/kids/page.jsx
@@ -1,7 +1,21 @@
+import ProductCard from '@/components/ProductCard'
 import React from 'react'
 
-export default function Kids() {
+export default async function Kids() {
+    const data = await fetch('http://localhost:4000/api/products?category=men')
+  const products = await data.json()
+  console.log(products)
+
+
   return (
-    <div>Kids</div>
+    <div className='container mx-auto py-10'>
+          <div className='grid gap-5 grid-col-1 md:grid-cols-3 lg:grid-cols-4'>
+          {
+            products.map(product => (
+              <ProductCard key={product._id} product={product}  ></ProductCard>
+            ))
+          }
+        </div>
+    </div>
   )
 }
--- src/app/women/page.jsx
+++ src/app/women/page.jsx
@@ -1,7 +1,21 @@
+import ProductCard from '@/components/ProductCard'
 import React from 'react'
 
-export default function Women() {
+export default async function Women() {
+    const data = await fetch('http://localhost:4000/api/products?category=women')
+  const products = await data.json()
+  console.log(products)
+
+
   return (
-    <div>Women</div>
+    <div className='container mx-auto py-10'>
+          <div className='grid gap-5 grid-col-1 md:grid-cols-3 lg:grid-cols-4'>
+          {
+            products.map(product => (
+              <ProductCard key={product._id} product={product}  ></ProductCard>
+            ))
+          }
+        </div>
+    </div>
   )
 }
